06aaa96a6617bcde5ffd8992c583dc9407945488
Sneh Acharya <60751508+sneh-ach@users.noreply.github.com>
2023-02-03 14:03:38 -0600

    Update frame.py

--- frame.py
+++ frame.py
@@ -3,7 +3,7 @@ import cv2
 import os
   
 # Read the video from specified path
-cam = cv2.VideoCapture("C:\\Users\\Admin\\PycharmProjects\\project_1\\openCV.mp4")
+cam = cv2.VideoCapture("path/for/file")
   
 try:
       
